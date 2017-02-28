934611bd9fcc019a5b68197bb5ec4dfb27a3657d
Chris Finney and Cameron Jensen <cameron@>
2017-02-27 16:07:15 -0800

    Add the remove function from the second form.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ function toDoItems(task) {
   this.task = task;
 }
 
+
+//User Interface
 $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
@@ -10,7 +12,25 @@ $(document).ready(function(){
 
     var honeyDo = new toDoItems(taskItem);
 
-    $("#listToDo ol").append("<li><span>" + honeyDo.task + "</span></li>");
+    $("#itemList").append("<div><input type='checkbox' name='task-items'>" + " " + "<span>" + honeyDo.task + "</span><br></div>");
+
+    $("#deleteList").show();
+    $("input#taskItem").val("");
+  });
+
+
+  $("#deleteList").submit(function(){
+    // debugger;
+    event.preventDefault();
+
+    $("#deleteList").show();
+
+    $("input:checkbox[name=task-items]:checked").each(function(){
+      // var eachItem = $(this).val();
+      $(this).parent().remove();
+      // $("#itemList").remove(eachItem);
+    });
+
   });
 
 });
